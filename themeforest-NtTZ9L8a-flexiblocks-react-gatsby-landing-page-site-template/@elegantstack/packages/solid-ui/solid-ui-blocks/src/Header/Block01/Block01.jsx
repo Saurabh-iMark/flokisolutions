44d2fa2b98722bf951cc555be2da1973f3c8004c
Saurabh Chauhan <saurabh.chauhan@imarkinfotech.com>
2023-05-03 16:30:30 +0530

    updates 7.2.5

--- themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/packages/solid-ui/solid-ui-blocks/src/Header/Block01/Block01.jsx
+++ themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/packages/solid-ui/solid-ui-blocks/src/Header/Block01/Block01.jsx
@@ -104,29 +104,29 @@ const HeaderBlock01 = ({ content: { images, collection }, menuJustify }) => {
                       backgroundColor: '#f2f2f2', padding: '10px',
                       boxShadow: '0 0 4px rgba(0, 0, 0, 0.2)',
                       zIndex: 1, borderColor: 'red', bordeWidth: 2,
-                      flexDirection: 'column', padding: '5%'
+                      flexDirection: 'column', padding: '2%'
                     }}>
 
                     <GLink  to="/services">
-                    <Box sx={{ paddingHorizontal: '60px', paddingVertical: '20px', borderBottom: '1px solid #ccc', cursor: "pointer" }}>
+                    <Box sx={{ borderBottom: '1px solid #ccc', cursor: "pointer" }}>
                       Service 1
                     </Box>
                     </GLink>
 
                     <GLink  to="/testpage">
-                    <Box sx={{ paddingHorizontal: '60px', paddingVertical: '20px', padding: '100px', borderBottom: '1px solid #ccc', cursor: "pointer" }}>
+                    <Box sx={{ borderBottom: '1px solid #ccc', cursor: "pointer" }}>
                       Service 2
                     </Box>
                     </GLink>
 
                     <GLink  to="/about-us">
-                    <Box sx={{ paddingHorizontal: '60px', paddingVertical: '20px', padding: '100px', borderBottom: '1px solid #ccc', cursor: "pointer" }}>
+                    <Box sx={{ borderBottom: '1px solid #ccc', cursor: "pointer" }}>
                       Service 3
                     </Box>
                     </GLink>
 
                     <GLink  to="/">
-                    <Box sx={{ paddingHorizontal: '60px', paddingVertical: '20px', padding: '100px', borderBottom: '1px solid #ccc', cursor: "pointer" }}>
+                    <Box sx={{ borderBottom: '1px solid #ccc', cursor: "pointer" }}>
                       Service 4
                     </Box>
                     </GLink>
