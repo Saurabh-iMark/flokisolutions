d14bce92d7a10fce755eed4c6057472a3f3b1d2b
Saurabh Chauhan <saurabh.chauhan@imarkinfotech.com>
2023-05-03 17:14:04 +0530

    updates 7.3.3

--- themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/packages/solid-ui/solid-ui-blocks/src/Header/Block01/Block01.jsx
+++ themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/packages/solid-ui/solid-ui-blocks/src/Header/Block01/Block01.jsx
@@ -71,6 +71,8 @@ const HeaderBlock01 = ({ content: { images, collection }, menuJustify }) => {
 
   console.log(collection);
   console.log(collection[0].buttons);
+  console.log(collection[0].dropdown);
+  
 
 
   return (
