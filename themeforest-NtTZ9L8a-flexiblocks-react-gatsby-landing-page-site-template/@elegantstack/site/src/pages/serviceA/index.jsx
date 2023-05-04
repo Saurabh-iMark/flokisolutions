dbe308fcda4cda5265425fb37921c7ecc2a9393f
Saurabh Chauhan <saurabh.chauhan@imarkinfotech.com>
2023-05-04 09:57:19 +0530

    7.4.5

--- themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/site/src/pages/serviceA/index.jsx
+++ themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/site/src/pages/serviceA/index.jsx
@@ -330,8 +330,93 @@ img{
   const MyHTMLComponent = () => {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>This is my HTML design in Gatsby.</p>
+      <div class="product1">
+        <div class="mycontainer">
+          <div class="sec1">
+            <h4>Our Services</h4>
+            <h1>Floki D2C App</h1>
+            <p>Take Control of Your Sales and Build a Direct Relationship with Your Customers!</p>
+          </div>
+        </div>
+      </div>
+      <div class="discrptn">
+         <div class="mycontainer"> 
+            <h4>Product Description</h4>
+            <p>Our Floki D2C App is a white label e-commerce solution designed specifically for product manufacturers and producers who want to sell directly to consumers. With our customizable and secure platform, you can easily launch your online store and start selling your products to a wider audience, while building a direct relationship with your customers. Our solution is fast, reliable, and customizable to meet your business needs.</p>
+         </div>
+      </div>
+      <div class="product1">  
+        <div class="mycontainer"> 
+          <div class="sect"> 
+            <div class="row">
+                <div class="colm6">
+                  <img src="./images/shutterstock_6.jpg" alt="product_img">
+                </div>
+                <div class="colm6">
+                  <h4>Key Features :</h4>
+                  <ul class="liststyle">
+                    <li>User-friendly interface that can be re-branded to match your brand identity</li>
+                    <li>Built-in reporting tools to help you understand customer behavior and preferences, so you can optimize your offerings and increase customer satisfaction and loyalty</li>
+                    <li>Flexible product catalog and inventory management tools to allow you to easily adjust your offerings and respond to changes in demand or new product releases</li>
+                    <li>Advanced marketing tools and integrations to enable personalized messaging and promotional campaigns to drive sales and engagement.</li>
+                    <li>Multi-language and currency support for a global reach</li>
+                    <li>Real-time order tracking and delivery notifications for you and your customers</li>
+                  </ul>
+                </div>
+            </div>
+          </div>
+          <div class="sect">
+            <div class="row">
+                <div class="colm6">
+                  <h4>Benefits :</h4>
+                  <ul class="liststyle">
+                    <li><strong>Take control of your sales and distribution</strong> by selling directly to consumers.</li>
+                    <li>Build a <strong>direct relationship with your customers</strong>, gain insights into their behavior, and tailor your offerings to their preferences.</li>
+                    <li><strong>Increase your revenue</strong> by reducing the cost of third-party sales channels and distributors.</li>
+                    <li>Enhance your <strong>brand value</strong> and increase <strong>customer loyalty</strong> by offering a seamless and personalized online shopping experience.</li>
+                  </ul>
+                </div>
+                <div class="colm6">
+                  <img src="./images/shutterstock_11.jpg" alt="product_img">
+                </div>
+            </div>
+          </div>
+          <div class="sect">
+            <div class="row">
+                <div class="colm6">
+                  <img src="./images/shutterstock_9.jpg" alt="product_img">
+                </div>
+                <div class="colm6">
+                  <h4>Use Cases :</h4>
+                  <ul class="liststyle">
+                    <li>A product manufacturer who wants to take control of their sales and distribution channels.</li>
+                    <li>A producer who wants to build a direct relationship with their customers and increase their revenue.</li>
+                    <li>A business owner who wants to enhance their brand value and increase customer loyalty by offering a seamless and personalized online shopping experience.</li>
+                  </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sec2">
+        <div class="mycontainer">
+          <div class="row">
+            <div class="colm4">
+              <h4>Deliverables</h4>
+              <p>User App</p>
+              <p>SuperAdmin Backend</p>
+            </div>
+            <div class="colm4">
+              <h4>Testimonials</h4>
+              <p>“Since we started using this Floki D2C platform, we've been able to take control of our sales and distribution channels, and build a direct relationship with our customers. The platform is easy to use, customizable, and secure, and our sales have increased significantly as a result." - Maisoon, E-commerce Director of Addoha Poultry..</p>
+            </div>
+            <div class="colm4">
+              <h4>Call-to-Action</h4>
+              <p>Take control of your sales and build a direct relationship with your customers with our Floki D2C App. Contact us to schedule a demo or sign up for a free trial.</p>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     );
   };
@@ -350,7 +435,7 @@ img{
       <Header content={content['header-light']} />
       <MyHTMLComponent></MyHTMLComponent>
       
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
       <Divider space='5' />
       <Footer content={content['footer']} />
     </Layout>
