34d73dcf0b8fdd8c92a2679c84ab7d1651dfedfb
Saurabh Chauhan <saurabh.chauhan@imarkinfotech.com>
2023-05-10 17:12:20 +0530

    8.5.6

--- themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/site/src/pages/floki-d2c-app/index.jsx
+++ themeforest-NtTZ9L8a-flexiblocks-react-gatsby-landing-page-site-template/@elegantstack/site/src/pages/floki-d2c-app/index.jsx
@@ -35,27 +35,8 @@ const ServiceAPage = props => {
 
 
   const MyHTMLComponent = () => {
-    return 
-
+    return (
       <div classNameName="service_spacer">
-
-        {/* <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> */}
-        {/* <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-      <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-      <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js"></script>
-      <script>
-        jQuery('.testimonial_slider').slick({
-            slidesToShow: 1,
-            infinite:false,
-            slidesToScroll: 1,
-            autoplay: true,
-            dots:true,
-            arrows:false,
-            autoplaySpeed: 2500
-        });
-      </script>
-     */}
-
       <div classNameName="product1">
         <div classNameName="mycontainer">
           <div classNameName="sec1">
